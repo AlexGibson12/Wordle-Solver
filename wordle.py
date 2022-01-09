46a029fa3e649b404b093b1bf4e38b32ba3ce1f9
AlexGibson12 <afg9000@gmail.com>
2022-01-09 19:17:26 +0000

    Added the ability to play

--- wordle.py
+++ wordle.py
@@ -1,3 +1,4 @@
+import random
 class Wordle:
     def __init__(self,words,possiblewords):
         self.words = words
@@ -21,7 +22,10 @@ class Wordle:
                 setlengths[colour] = 1
             else:
                 setlengths[colour]+=1
-        maximum = max([setlengths[i] for i in setlengths.keys()])
+        if len(setlengths.keys()) > 0:
+            maximum = max([setlengths[i] for i in setlengths.keys()])
+        else:
+            maximum = 1
         return maximum
     def getnextword(self):
         minmaximalwords = -1
@@ -47,21 +51,44 @@ class Wordle:
                 wordstoerase.append(possibility)
         for word in wordstoerase:
             self.possiblewords.remove(word)
+client = input("Play or solve? P/S")
+while client.lower() not in ['p','s']:
+    client = input("Play or solve? P/s")
+playing = (client.lower() == 'p')
 
 f = open("words.txt","r")
 words = [i.strip() for i in f.readlines()]
 f = open("possiblewords.txt","r")
 possiblewords = [i.strip() for i in f.readlines()]
 wordle = Wordle(words,possiblewords)
-currentword = "serai"
-while True:
-    print(currentword)
-    colourcode = input("Colour Combination:")
-    wordle.play(currentword,colourcode)
-    savedword = currentword
-    currentword = wordle.getnextword()
-    if currentword==savedword:
-        break
-print(f'Success! The word is {currentword}')
+maxguesses = 6
+if playing:
+    guesses = 0
+    correctword = random.choice(possiblewords)
+
+    while guesses<=6:
+
+        guesses+=1
+        guess = input("What is your guess?")
+        colours = wordle.ComputeColours(guess,correctword)
+        print(colours)
+        print(guess)
+        if guess == correctword:
+            break
+    if guesses<=6:
+        print("Well done!")
+    else:
+        print("Too many guesses")
+else:
+    currentword = "serai"
+    while True:
+        print(currentword)
+        colourcode = input("Colour Combination:")
+        wordle.play(currentword,colourcode)
+        savedword = currentword
+        currentword = wordle.getnextword()
+        if currentword==savedword:
+            break
+    print(f'Success! The word is {currentword}')
     
 
